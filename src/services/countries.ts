29f49f0e6327b5d6f4e0f751ef9cce863085678c
Timer00 <theocarraraleao@gmail.com>
2024-05-01 15:55:31 +0100

    use nextJS fetch for auto cache

--- src/services/countries.ts
+++ src/services/countries.ts
@@ -1,11 +1,15 @@
-import axios from "axios";
 import { type Country } from "@/interfaces/countries";
 import { mapCountriesToLocales } from "@/mappers/countries";
 
 export const fetchCountries = async (): Promise<(Country & { localeCode?: string })[]> => {
   try {
-    const response = await axios.get('https://retoolapi.dev/TkEl3I/countriesdata');
-    const countries = response.data as Country[];
+    const response = await fetch('https://retoolapi.dev/TkEl3I/countriesdata');
+
+    if (!response.ok) {
+      return Promise.reject(Error("Failed to fetch countries"));
+    }
+
+    const countries = await response.json() as Country[];
     return mapCountriesToLocales(countries);
   } catch (error) {
     console.error('Error fetching countries:', error);
